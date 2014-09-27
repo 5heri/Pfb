3d2f19f1b9710e84ed27ba7b70bed34156d38bce
5heri <sher.khan12@imperial.ac.uk>
2014-09-28 02:11:42 +0500

    UTree

---
+++ Utree.java
@@ -0,0 +1,28 @@
+import java.util.scanner;
+
+public class Utree {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int testCases = in.nextInt();
+
+		while (testCases > 0) {
+			System.out.println(heightAfterCycles(in.nextInt()));
+			testCases--;
+		}
+	}
+
+	private static int heightAfterCycles(int cycles) {
+		int height = 1;
+
+		for (int i = 0; i < cycles; i++) {
+			if (i % 2 == 0) {
+				height *= 2;
+			} else {
+				height++;
+			}
+		}
+		return height;
+	}
+}
+
